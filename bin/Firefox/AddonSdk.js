8becbe9fdfc32a1149d235d50594e212fb6ce731
OffByNone <jeremy.mcneil@gmail.com>
2015-08-13 20:15:06 -0500

    Foolishly didn't run it before i checked it in.

--- bin/Firefox/AddonSdk.js
+++ bin/Firefox/AddonSdk.js
@@ -76,7 +76,7 @@ module.exports.url = function () {
 	return require('sdk/url');
 }; // https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/url
 module.exports.btoa = function () {
-	return require("api-utils/base64").encode;
+	return require("sdk/base64").encode;
 }; //https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/base64
 module.exports.getNativeWindowMenu = function () {
 	return Services.wm.getMostRecentWindow("navigator:browser").NativeWindow.menu;
